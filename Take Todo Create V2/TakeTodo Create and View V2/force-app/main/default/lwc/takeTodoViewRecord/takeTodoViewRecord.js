d313a32093daa6be1889ba95f3bf1c3d64d52db9
devlynx5 <44241466+devlynx5@users.noreply.github.com>
2021-05-04 08:49:48 -0400

    Add files via upload

---
+++ Take Todo Create V2/TakeTodo Create and View V2/force-app/main/default/lwc/takeTodoViewRecord/takeTodoViewRecord.js
@@ -0,0 +1,21 @@
+import { LightningElement, wire } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import Phone_FIELD from '@salesforce/schema/Account.Phone';
+import TYPE_Field from '@salesforce/schema/Account.Type';
+import CreatedDate_Field from '@salesforce/schema/Account.CreatedDate';
+import gettodo from '@salesforce/apex/takeTodoViewRecordController.gettodo';
+const COLUMNS = [
+    { label: 'Todo  Name', fieldName: NAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Phone ', fieldName: Phone_FIELD.fieldApiName, type: 'text' },
+    { label: 'Type',fieldName: TYPE_Field.fieldApiName, type: 'text' },
+    { label: 'Created Date', fieldName: CreatedDate_Field.fieldApiName, type: 'Date/Time' }
+];
+
+export default class TakeTodoViewRecord extends LightningElement {
+
+    
+    columns = COLUMNS;
+    @wire(gettodo)
+    accounts;
+
+}
